14c4d95b565fa5c29bbd008c2f3aee205883c6b5
Bryan <bryannsanchez1234@gmail.com.com>
2022-03-28 08:09:32 -0700

    started server.js

---
+++ routes/UsedCars.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+
+
+// get all
+router.get('/', (req, res) => {
+
+})
+
+// get one
+router.get('/:id', (req, res) => {
+  
+})
+// create one
+
+// update one
+
+// delete one
+
+
+module.exports = router
