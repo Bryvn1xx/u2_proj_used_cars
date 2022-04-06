42df01d135655b8abfaa71e071f2b13db7a650d8
Bryan <bryannsanchez1234@gmail.com.com>
2022-04-06 13:41:50 -0700

    fixing errors

--- client/src/components/UpdateCar.js
+++ client/src/components/UpdateCar.js
@@ -1,7 +1,6 @@
 import { React, useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-// import Car from '../Models/Cars'
 const UpdateCar = (props) => {
 
 
